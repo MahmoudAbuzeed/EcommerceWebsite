749c0a5db89c84dfdd9a809bbcc7f766c30f52f8
Mahmoud Abuzeed <mahmoudabozaid56@gmail.com>
2020-11-21 06:35:10 +0200

    fix some bugs

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -30,12 +30,12 @@ exports.requireSignin = (req, res, next)=> {
 
 
 
-/*exports.userMiddleware = (req, res, next) => {
+exports.userMiddleware = (req, res, next) => {
     if(req.user.role !== 'user'){
         return res.status(400).json({ message: 'User access denied' })
     }
     next();
-}*/
+}
 
 exports.adminMiddleware = (req, res, next) => {
     if(req.user.role !== 'admin'){
